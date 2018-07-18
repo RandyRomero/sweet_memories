19edb3ffb07ac8990d32ca1de651c5fc7a926f3c
RandyRodriguez <a.ya.miheev@gmail.com>
2018-07-18 14:52:54 +0300

    Add functionality to single out photos that were taken in this day in the all previous years by reading date from exif if there is no date in filename

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 PHOTO_ARCHIVE = r'F:\AllPhoto'  # Place where to pick up photos from
+# PHOTO_ARCHIVE = r'C:\ya.disk\YandexDisk\Фотокамера'  # Place where to pick up photos from
 SCREENSAVER_FOLDER = r'C:\screensaver'  # Place where to put chosen photos
 INITIAL_NUMBER_OF_PHOTOS = 100  # Number of photos to pick initially
 NUMBER_OF_PHOTOS_TO_ADD = 5  # Number of photo to add after some time
--- sweet_memories.py
+++ sweet_memories.py
@@ -6,12 +6,23 @@ import shutil
 import random
 import time
 import config  # Config need to store path to folders to work to
+import exifread
+import re
+from datetime import datetime
 
 print('Hi there!')
 
 all_copied_files = []  # Keep track of copied photos in order to delete them in order they were copied
 total_photo_size = 0  # Keep track of size of photos that were copied in order to remove some when there are to much
 # of them
+
+
+# Read EXIF from file to find date when photo was taken
+def get_date_from_exif(root, file):
+    with open(os.path.join(root, file), 'rb') as f:
+        exif_info = exifread.process_file(f, details=False)
+        return (str(exif_info.get('EXIF DateTimeOriginal', '')) or str(exif_info.get('EXIF DateTimeDigitized', '')) or
+                str(exif_info.get('Image DateTime', '')) or 'no data')
 
 
 # Delete first photos that were copied to clean up a bit
@@ -34,6 +45,41 @@ def delete_photos():
     print('after')
     print(f'Done cleaning screensaver folder. There are {(total_photo_size / 1024**2):.0f} MB of photos ready to show.')
     return
+
+
+# Look for photos that were taken in that day in previous years2
+def get_photo_of_the_day(archive_path):
+    list_of_pictures = []
+    files_without_date_in_name = 0
+    for root, subfolders, files in os.walk(archive_path):
+        print(f'Checking {root}')
+        # Get regex with any year and today's date like \d{4}-07-16
+        regex = re.compile('\d{4}-' + str(datetime.now().date())[5:])
+        for file in files:
+            if not file.lower().endswith('.jpg') or file.lower().endswith('.jpeg'):
+                continue
+
+            # Use regex to look for photos with specific date: first check if there is date in filename,
+            # if not check the date in EXIF data within photo
+            if not re.match(r'\d{4}-\d{2}-\d{2}', file):
+                print(f'Checking {file}...')
+                data = get_date_from_exif(root, file)
+                print(data)
+                today = str(datetime.now().date()).replace('-', ':')[5:]
+                if re.match(r'\d{4}:' + today, data):
+                    files_without_date_in_name += 1
+                    print(f'Files without name in date: {files_without_date_in_name}')
+                    list_of_pictures.append(os.path.join(root, file))
+                    continue
+
+            if re.match(regex, file):
+                list_of_pictures.append(os.path.join(root, file))
+
+    for pic in list_of_pictures:
+        print(pic)
+    print(f'There are {len(list_of_pictures)} pictures.')
+    print(f'There are {files_without_date_in_name} pictures were found by examining of EXIF.')
+    return list_of_pictures
 
 
 # Look through the whole archives with photos and create a list of all existing files with jpg extension
@@ -66,25 +112,32 @@ def copy_photos(photos):
     print(f'Done! There are {(total_photo_size / 1024**2):.0f} MB of photos ready to show.')
 
 
+def create_folder():
+    if os.path.exists(config.SCREENSAVER_FOLDER):
+        shutil.rmtree(config.SCREENSAVER_FOLDER)
+    for attempt in range(5):
+        try:
+            os.mkdir(config.SCREENSAVER_FOLDER)
+            return True
+        except PermissionError:
+            time.sleep(1)
+            continue
+    else:
+        print('Can\'t create folder for photos')
+        return False
+
+
 # This chunk of code let user to choose mode of how script will work
 # Also it is responsible for adding photos after given amount of time
 while True:
     print('Choose one option below:\n1. Copy 5 random photos.\n2. Copy photos that were taken in this day in the past')
     option = int(input('Please type "1" or "2": '))
+
     if option == 1:
         pics = get_list_of_photos(config.PHOTO_ARCHIVE)
 
-        if os.path.exists(config.SCREENSAVER_FOLDER):
-            shutil.rmtree(config.SCREENSAVER_FOLDER)
-        for i in range(5):
-            try:
-                os.mkdir(config.SCREENSAVER_FOLDER)
-                break
-            except PermissionError:
-                time.sleep(1)
-                continue
-        else:
-            print('Can\'t create folder for photos')
+        if not create_folder():
+            exit()
 
         copy_photos(random.sample(pics, config.INITIAL_NUMBER_OF_PHOTOS))
 
@@ -94,8 +147,13 @@ while True:
                 delete_photos()
             copy_photos(random.sample(pics, config.NUMBER_OF_PHOTOS_TO_ADD))
 
-    if option == 2:
-        print('Ooops! This feature isn\'t available yet.')
-        continue
+    elif option == 2:
+        pics = get_photo_of_the_day(config.PHOTO_ARCHIVE)
+
+        if not create_folder():
+            exit()
+
+        copy_photos(pics)
+        break
     else:
         print('Input error. You can only type "1" or "2". Try again.')
